3310d1e15a2160eccbde31f15d8f9c9bb090f4ac
Anooja M <anoojam@jabirs-MacBook-Air.local>
2025-04-11 14:01:15 +0530

    json server deployment done

---
+++ api/json.js
@@ -0,0 +1,13 @@
+// api/json.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = (req, res) => {
+  // let json-server handle the request
+  server(req, res);
+};
